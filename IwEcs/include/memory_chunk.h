f9e544252ec7a37e3af85d163a1a1f2c0bc6c78d
Iain Winter <iainwinter1@gmail.com>
2018-11-09 01:52:19 -0500

    Broke mem chunk

--- IwEcs/include/memory_chunk.h
+++ IwEcs/include/memory_chunk.h
@@ -1,19 +1,22 @@
 #pragma once
 
 #include "iwecs.h"
+#include <tuple>
 
-template<typename T>
+template<typename... T>
 class memory_chunk {
 private:
-	T* m_memory;
-	T* m_free;
-	T* m_end;
+	using tup_t = std::tuple<T...>;
+
+	tup_t* m_memory;
+	tup_t* m_free;
+	tup_t* m_end;
 	unsigned int m_size;
 	unsigned int m_count;
 public:
 	memory_chunk(int count)
-		: m_size(count * sizeof(T)),
-		m_memory(new T[m_size]),
+		: m_size(count * sizeof(tup_t)),
+		m_memory(new tup_t[count]),
 		m_free(m_memory),
 		m_end(m_memory + count) {
 	}
@@ -22,9 +25,9 @@ public:
 		delete[] m_memory;
 	}
 
-	bool alloc(T&& data) {
+	bool alloc(T&&... data) {
 		if (m_free != m_end) {
-			*m_free = T(std::move(data));
+			*m_free = std::make_tuple(std::move(data)...);
 			m_free++;
 			m_count++;
 
@@ -34,9 +37,9 @@ public:
 		return false;
 	}
 
-	bool alloc(T& data) {
+	bool alloc(T&... data) {
 		if (m_free != m_end) {
-			*m_free = T(data);
+			*m_free = std::make_tuple(data...);
 			m_free++;
 			m_count++;
 
--- Sandbox/src/Source.cpp
+++ Sandbox/src/Source.cpp
@@ -4,6 +4,7 @@
 #include "IwEngine/IwEngine.h"
 
 #include "IwEcs/memory_chunk.h"
+#include <tuple>
 
 class Game : public IwEngine::Application {
 public:
@@ -11,8 +12,8 @@ public:
 	~Game() {}
 
 	void Run() override {
-		memory_chunk<int> c = memory_chunk<int>(64);
-		while (c.alloc(5)) {};
+		memory_chunk<int, float> c = memory_chunk<int, float>(64);
+		c.alloc(5, 5.5f);
 	}
 };
 
