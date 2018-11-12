59535a5c141ae19c0950957901851405d022c749
Iain Winter <iainwinter1@gmail.com>
2018-11-12 03:41:40 -0500

    Removed IwEcs1

--- IwEcs1/include/iwecs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef IWECS_EXPORT
-#	define IWECS_API __declspec(dllexport)
-#else
-#	define IWECS_API __declspec(dllimport)
-#endif
-
-namespace iwecs {}
--- IwEcs1/src/memory_chunk.cpp
+++
@@ -1,5 +0,0 @@
-#include "..\include\memory_chunk.h"
-
-void iwecs::t::tr() {
-	int i = 5 + 3;
-}
