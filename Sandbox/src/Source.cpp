187eae4a37914dd2a52cc0e800d01ce21b44906a
Iain Winter <iainwinter1@gmail.com>
2019-02-08 02:31:13 -0500

    Testing

--- Sandbox/src/Source.cpp
+++ Sandbox/src/Source.cpp
@@ -1,14 +1,10 @@
 #include "Core/EntryPoint.h"
-#include "set/sparse_set.h"
+#include "component/component_map.h"
 
 #include <iostream>
 
 struct Position {
-	float x, y;
-};
-
-struct Velocity {
-	float x, y;
+	int x, y;
 };
 
 class Game : public IwEngine::Application {
@@ -17,27 +13,20 @@ public:
 	~Game() {}
 
 	void Run() override {
-		iwu::sparse_set<int, Position> set = iwu::sparse_set<int, Position>();
-		set.insert(3);
-		set.insert(1);
-		set.insert(0);
-		set.erase(1);
+		iwecs::component_map world = iwecs::component_map();
 
-		auto itr = set.begin();
-
-		//iwecs::component_map world = iwecs::component_map();
-
-		//world.emplace<Position>(0, Position {1, 2});
+		world.emplace<Position>(0, Position {1, 2});
 		//world.erase<Position>(0);
 		//world.emplace<Position, Velocity>(0, Position{ 1, 2 }, Velocity{3, 4});
 		//world.erase(0);
+
+		std::cin.get();
 	}
 };
 
 IwEngine::Application* CreateApplication() {
 	return new Game();
 }
-
 
 //iwm	iwmath
 //iwecs	iwecs
