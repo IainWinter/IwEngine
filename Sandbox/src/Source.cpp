6c3411b66facf091d347ed9d8f863f265879939d
Iain Winter <iainwinter1@gmail.com>
2019-03-02 05:06:37 -0500

    Testing updates

--- Sandbox/src/Source.cpp
+++ Sandbox/src/Source.cpp
@@ -1,36 +1,34 @@
-#include "Core/EntryPoint.h"
-#include "space/space.h"
-
 #include <iostream>
+#include "EntryPoint.h"
 
-struct Position { int x, y;  };
-struct Velocity { int x, y;  };
-struct Mesh     { int verts; };
-struct Collider { int verts; };
-struct Player   {};
-struct Bullet   {};
-struct Asteroid { int level; };
-struct Score    { int score; };
-
-void physics_update(iwent::view<Position, Velocity, Collider>& view) {
-	
-}
+#include "logger.h"
 
 class Game : public IwEngine::Application {
 public:
+	Game() {}
+
+	void Initilize() {
+		IwEngine::Application::Initilize();
+	}
+
+	void Start() {
+		IwEngine::Application::Start();
+	}
+
+	void Stop() {
+		IwEngine::Application::Stop();
+	}
+
+	void Destroy() {
+		IwEngine::Application::Destroy();
+	}
+
 	void Run() override {
-		iwent::space<Game> space;
-		space.make_common<Position>();
-		space.make_subspace<Velocity, Collider, Player, Asteroid>();
-		space.make_subspace<Mesh>();
+		window.Open();
 
-		space.make_action<Position, Velocity, Collider>(physics_update);
-
-		auto e = space.create();
-		space.assign<Position>(e, Position{ 1, 2 });
-		space.assign<Velocity>(e, Velocity{ 1, 2 });
-		space.assign<Collider>(e, Collider{ 5 });
-		space.assign<Mesh>    (e, Mesh{ 5 });
+		MiniLog::current_level() = logINFO;
+		MINILOG(logDEBUG) << "DEBUG Log message";
+		MINILOG(logINFO) << "INFO Log message";
 
 		std::cin.get();
 	}
@@ -39,11 +37,3 @@ public:
 IwEngine::Application* CreateApplication() {
 	return new Game();
 }
-
-//iwm	iwmath
-//iwecs	iwecs
-//iwevt	iwevents
-//iwi	iwinput
-//iwe	iwengine
-//iwp	iwphysics
-//iwa	iwaudio
