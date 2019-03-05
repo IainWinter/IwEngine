aea9e3fc5e8fbe0452f6b3e8bb39d1b4fedea6f1
Iain Winter <iainwinter1@gmail.com>
2019-03-05 02:28:45 -0500

    Updated tests

--- Sandbox/src/Source.cpp
+++ Sandbox/src/Source.cpp
@@ -1,19 +1,20 @@
 #include "EntryPoint.h"
-#include "log.h"
-
-#include <iostream>
 
 class Game : public IwEngine::Application {
 public:
-	Game() {}
 
-	void Start() override {
-		LOG_INFO("This is a test");
-
-		IwEngine::Application::Start();
-	}
 };
 
 IwEngine::Application* CreateApplication() {
-	return new Game();
+	IwEngine::WindowOptions options {
+		1280,
+		720,
+		IwEngine::NORMAL,
+		true,
+	};
+
+	Game* game = new Game();
+	game->Initilize(options);
+
+	return game;
 }
