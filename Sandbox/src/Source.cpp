ebbeec9422ddd23c04a1d487780f7f576696674e
Iain Winter <iainwinter1@gmail.com>
2019-03-06 03:00:13 -0500

    Updated tests

--- Sandbox/src/Source.cpp
+++ Sandbox/src/Source.cpp
@@ -3,16 +3,54 @@
 #include <gl/glew.h>
 #include <gl/wglew.h>
 
+class TestLayer : public IwEngine::Layer {
+public:
+	TestLayer()
+		: IwEngine::Layer("Test") 
+	{}
+
+	int Initilize() override {
+		LOG_DEBUG << "Test Layer initilizing";
+		return 0;
+	}
+
+	void Start() override {
+		LOG_DEBUG << "Test Layer started";
+	}
+
+	void Stop() override {
+		LOG_DEBUG << "Test Layer stopped";
+	}
+
+	void Destroy() override {
+		LOG_DEBUG << "Test Layer destroyed";
+	}
+
+	void Update() override {
+		LOG_INFO << "Test Layer updated";
+	}
+
+	void OnEvent(IwEngine::Event& e) override {
+		LOG_DEBUG << "Test Layer received event " << e.type;
+	}
+};
+
 class Game : public IwEngine::Application {
 public:
+	int Initilize(
+		IwEngine::WindowOptions& options) override
+	{
+		PushLayer(new TestLayer());
+
+		return Application::Initilize(options);
+	}
+
 	void Start() override {
 		glClearColor(0.5, 0.3, 0.87, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		window.Render();
 
-		space.Make_Subspace("Physics");
-		space.Make_Subspace("Renderer");
 
 		IwEngine::Application::Start();
 	}
@@ -27,7 +65,9 @@ IwEngine::Application* CreateApplication() {
 	};
 
 	Game* game = new Game();
-	game->Initilize(options);
+	game->Initilize(options); //Not where it should be
 
 	return game;
 }
+
+
