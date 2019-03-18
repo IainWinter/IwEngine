d33f1380038f71d17cd7e128a95ae965b0f8b2bc
Iain Winter <iainwinter1@gmail.com>
2019-03-18 03:04:17 -0400

    Updated tests

--- Sandbox/src/Source.cpp
+++ Sandbox/src/Source.cpp
@@ -1,10 +1,12 @@
 #include "iw/engine/EntryPoint.h"
 #include "iw/engine/ImGui/ImGuiLayer.h"
+#include "iw/engine/Entity/EntityLayer.h"
 
 class Game : public IwEngine::Application {
 public:
 	Game() {
 		PushOverlay(new IwEngine::ImGuiLayer());
+		PushLayer(new IwEngine::EntityLayer());
 	}
 };
 
