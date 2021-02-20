02585aa47e169e3467bf0e94bdbee01aad8751c2
Iain <Iain@DESKTOP-O9UC60V>
2021-02-19 23:21:51 -0500

    faster for only editing shaders

--- copyjustshaders.bat
+++ copyjustshaders.bat
@@ -1,1 +1,3 @@
 @xcopy C:\dev\IwEngine\Editor\assets\shaders C:\dev\IwEngine\Editor\build\assets\shaders\  /e /y /f /i
+@xcopy C:\dev\IwEngine\Editor\assets\shaders C:\dev\IwEngine\Editor\bin\Debug.windows.x86_64\assets\shaders\ /e /y /f /i
+@xcopy C:\dev\IwEngine\Editor\assets\shaders C:\dev\IwEngine\Editor\bin\Release.windows.x86_64\assets\shaders\ /e /y /f /i
