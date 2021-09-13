581cee2bc64ca2bcb05b4b240395f7fb6089b464
Iain <Iain@DESKTOP-O9UC60V>
2021-09-13 15:18:24 -0400

    ingnore header file in libs that get downloaded

--- Bootstrap.bat
+++ Bootstrap.bat
@@ -4,20 +4,22 @@ if not exist .\_download\ (
 	mkdir .\_download
 )
 
-if not exist .\IwEngine\extern\fmod\bin\ (
+if not exist .\IwEngine\extern\fmod\ (
 	echo Downloading FMod libs
-
-	.\Tools\curl.exe -sS https://winter.dev/engine/fmod_libs.zip -o .\_download\fmod_libs.zip
-	.\Tools\tar.exe -x -f .\_download\fmod_libs.zip -C .\IwEngine\extern\fmod
+	
+	mkdir .\IwEngine\extern\fmod
+	.\Tools\curl.exe -sS https://winter.dev/engine/fmod_win86.zip -o .\_download\fmod_win86.zip
+	.\Tools\tar.exe -x -f .\_download\fmod_win86.zip -C .\IwEngine\extern\fmod
 ) else (
 	echo FMod libs already installed
 )
 
-if not exist .\IwEngine\extern\assimp\bin\ (
+if not exist .\IwEngine\extern\assimp\ (
 	echo Downloading Assimp libs
 
-	.\Tools\curl.exe -sS https://winter.dev/engine/assimp_libs.zip -o .\_download\assimp_libs.zip
-	.\Tools\tar.exe -x -f .\_download\assimp_libs.zip -C .\IwEngine\extern\assimp
+	mkdir .\IwEngine\extern\assimp
+	.\Tools\curl.exe -sS https://winter.dev/engine/assimp_win86.zip -o .\_download\assimp_win86.zip
+	.\Tools\tar.exe -x -f .\_download\assimp_win86.zip -C .\IwEngine\extern\assimp
 ) else (
 	echo Assimp libs already installed
 )
---
+++ IwTooling/Bootstrap.bat
@@ -0,0 +1,21 @@
+@echo off
+
+if not exist .\_download\ (
+	mkdir .\_download
+)
+
+if not exist .\extern\llvm\ (
+	echo Downloading LLVM libs
+
+	mkdir .\extern\llvm
+	..\Tools\curl.exe -sS https://winter.dev/engine/llvm_win86.zip -o .\_download\llvm_win86.zip
+	..\Tools\tar.exe -x -f .\_download\llvm_win86.zip -C .\extern\llvm
+) else (
+	echo LLVM libs already installed
+)
+
+if exist .\_download\ (
+	rmdir /S /Q .\_download
+)
+
+echo Done
